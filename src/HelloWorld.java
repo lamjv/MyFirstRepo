09b3be6771df902d7cc5e9f654fc2ac6a0d58911
Jasmine Lam <lamjv@miamioh.edu>
2020-05-02 23:51:00 -0400

    My first commit in Git

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * Jasmine Lam
+ * CSE 271, Section A
+ * 5/2/2020
+ * Lab 13 - Using GitHub
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
